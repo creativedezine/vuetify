58b6619253429a77865cca07e95ca09c49010009
creative_dezine <creative_dezine@hotmail.com>
2019-02-23 21:55:44 +0500

    Frist Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer app floating dark v-model="drawer">
+      <v-list>
+          <v-list-tile>
+            <v-list-tile-avatar color="black">
+              <v-img
+                src="https://www.kamaliakhaddi.com/images/website/logos/logo1-new.png"
+                height=100%
+              />
+            </v-list-tile-avatar>
+            <v-list-tile-title class="title text-uppercase white--text">
+              <span class="font-weight-light">Kamalia</span>
+              <span> Khaddi</span>
+            </v-list-tile-title>
+          </v-list-tile>
+      </v-list>
+        <v-divider></v-divider>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to=" link.route ">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">
+              {{ link.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        { icon: 'home', text: 'Home', route: '/' },
+        { icon: 'reorder', text: 'Orders', route: '/orders' },
+        { icon: 'class', text: 'Products', route: '/products' },
+        { icon: 'settings', text: 'Settings', route: '/setting' }
+      ]
+    }
+  },
+  created () {
+    this.openDrawer()
+  },
+  methods: {
+    openDrawer: function () {
+      this.drawer = true
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,101 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row>
+      <v-flex xs12 md3>
+            <v-card color="purple" class="white--text">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">23</div>
+                      <div>Total Products</div>
+                      <div>(2013)</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                    height="125px"
+                    contain
+                  ></v-img>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-flex>
+      <v-flex xs12 md3>
+            <v-card color="purple" class="white--text">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">23</div>
+                      <div>Total Products</div>
+                      <div>(2013)</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                    height="125px"
+                    contain
+                  ></v-img>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 md3>
+            <v-card color="purple" class="white--text">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">23</div>
+                      <div>Total Products</div>
+                      <div>(2013)</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                    height="125px"
+                    contain
+                  ></v-img>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 md3>
+            <v-card color="purple" class="white--text">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">23</div>
+                      <div>Total Products</div>
+                      <div>(2013)</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                    height="125px"
+                    contain
+                  ></v-img>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-flex>
+
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
